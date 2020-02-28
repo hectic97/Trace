2981a612bf54c899cabda901f3abe7b43318ab90
hectic97 <hecticlife@g.skku.edu>
2020-02-28 10:45:33 +0900

    == Today Commit ==

--- C_practice/Baekjoon/p.c
+++ C_practice/Baekjoon/p.c
@@ -1,31 +1,82 @@
 #include <stdio.h>
-typedef struct
+#include <stdlib.h>
+typedef struct _loc
 {
-	int weight;
-	int height;
-	int rank;
-}Info;
+	int x;
+	int y;
+}Loc;
+void MergeTwoArea(Loc arr[], int left, int mid, int right)
+{
+	int fIdx = left;
+	int rIdx = mid + 1;
+	int i;
+
+	Loc* sortArr = (Loc*)malloc(sizeof(Loc) * (right + 1));
+	int sIdx = left;
+
+	while (fIdx <= mid && rIdx <= right)
+	{
+		if (arr[fIdx].y < arr[rIdx].y)
+			sortArr[sIdx] = arr[fIdx++];
+		else if (arr[fIdx].y == arr[rIdx].y && arr[fIdx].x < arr[rIdx].x)
+			sortArr[sIdx] = arr[fIdx++];
+		else if (arr[fIdx].y == arr[rIdx].y && arr[fIdx].x > arr[rIdx].x)
+			sortArr[sIdx] = arr[rIdx++];
+
+		else
+			sortArr[sIdx] = arr[rIdx++];
+		sIdx++;
+	}
+
+
+
+	if (fIdx > mid)
+	{
+		for (i = rIdx; i <= right; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+	else
+	{
+
+		for (i = fIdx; i <= mid; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+	for (i = left; i <= right; i++)
+	{
+		arr[i] = sortArr[i];
+	}
+	free(sortArr);
+}
+void MergeSort(Loc* arr, int left, int right)
+{
+	int mid;
+
+	if (left < right)
+	{
+		mid = (left + right) / 2;
+
+		MergeSort(arr, left, mid);
+		MergeSort(arr, mid + 1, right);
+
+		MergeTwoArea(arr, left, mid, right);
+	}
+}
 
 int main(void)
 {
-	int i, iter,j;
-	Info info[50];
+
+	int i, iter, n;
 	scanf("%d", &iter);
+	Loc* locations = (Loc*)malloc(sizeof(Loc) * iter);
 	for (i = 0; i < iter; i++)
 	{
-		scanf("%d %d", &info[i].weight, &info[i].height);
-		info[i].rank = 1;
+		scanf("%d %d", &locations[i].x, &locations[i].y);
 	}
+	MergeSort(locations, 0, iter - 1);
 	for (i = 0; i < iter; i++)
 	{
-		for (j = 0; j < iter; j++)
-		{
-			if (info[i].weight < info[j].weight&& info[i].height < info[j].height)
-				(info[i].rank)++;
-		}
+		printf("%d %d\n", locations[i].x, locations[i].y);
 	}
-	for (i = 0; i < iter; i++)
-		printf("%d ", info[i].rank);
 	return 0;
 
 }
--- C_practice/Proj/BST.c
+++ C_practice/Proj/BST.c
@@ -1,23 +1,82 @@
 #include <stdio.h>
-typedef struct _empInfo
+#include <stdlib.h>
+typedef struct _loc
 {
-	int empNum;
-	int age;
+	int x;
+	int y;
+}Loc;
+void MergeTwoArea(Loc arr[], int left, int mid, int right)
+{
+	int fIdx = left;
+	int rIdx = mid + 1;
+	int i;
 
-}EmpInfo;
+	Loc* sortArr = (Loc*)malloc(sizeof(Loc) * (right + 1));
+	int sIdx = left;
+
+	while (fIdx <= mid && rIdx <= right)
+	{
+		if (arr[fIdx].x < arr[rIdx].x)
+			sortArr[sIdx] = arr[fIdx++];
+		else if (arr[fIdx].x == arr[rIdx].x && arr[fIdx].y < arr[rIdx].y)
+			sortArr[sIdx] = arr[fIdx++];
+		else if (arr[fIdx].x == arr[rIdx].x && arr[fIdx].y > arr[rIdx].y)
+			sortArr[sIdx] = arr[rIdx++];
+
+		else
+			sortArr[sIdx] = arr[rIdx++];
+		sIdx++;
+	}
+
+
+
+	if (fIdx > mid)
+	{
+		for (i = rIdx; i <= right; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+	else
+	{
+
+		for (i = fIdx; i <= mid; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+	for (i = left; i <= right; i++)
+	{
+		arr[i] = sortArr[i];
+	}
+	free(sortArr);
+}
+void MergeSort(Loc* arr, int left, int right)
+{
+	int mid;
+
+	if (left < right)
+	{
+		mid = (left + right) / 2;
+
+		MergeSort(arr, left, mid);
+		MergeSort(arr, mid + 1, right);
+
+		MergeTwoArea(arr, left, mid, right);
+	}
+}
 
 int main(void)
 {
-	EmpInfo empInfoArr[1000];
-	EmpInfo ei;
-	int eNum;
-	printf("사번과 나이 입력: ");
-	scanf("%d %d", &(ei.empNum), &(ei.age));
-	empInfoArr[ei.empNum] = ei;
 
-	printf("확인하고픈 직원의 사번 입력 : ");
-	scanf("%d", &eNum);
-	ei = empInfoArr[eNum];
-	printf("사번 %d 나이 %d\n", ei.empNum, ei.age);
+	int i, iter, n;
+	scanf("%d", &iter);
+	Loc* locations = (Loc*)malloc(sizeof(Loc) * iter);
+	for (i = 0; i < iter; i++)
+	{
+		scanf("%d %d", &locations[i].x, &locations[i].y);
+	}
+	MergeSort(locations, 0, iter - 1);
+	for (i = 0; i < iter; i++)
+	{
+		printf("%d %d\n", locations[i].x, locations[i].y);
+	}
 	return 0;
+
 }

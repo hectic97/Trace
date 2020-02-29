a758f51f7e723ac8f62349a1ce2f2e30b541ed2c
hectic97 <hecticlife@g.skku.edu>
2020-02-29 23:26:59 +0900

    == Today Commit ==

--- C_practice/Baekjoon/p.c
+++ C_practice/Baekjoon/p.c
@@ -1,137 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
-
-typedef struct _node
+int Isinpl(int cx, int cy, int cr, int x, int y)
 {
-	int data;
-	struct _node* next;
-	struct _node* before;
-}Node;
-typedef struct _stack
-{
-	Node* head;
-	Node* tail;
-	int numOfData;
-
-}Stack;
-
-void InitStack(Stack* st)
-{
-	st->head = NULL;
-	st->tail = NULL;
-	st->numOfData = 0;
-}
-
-void Push(Stack* st, int data)
-{
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->data = data;
-	newNode->next = NULL;
-	
-	if (st->numOfData == 0)
-	{
-		st->head = newNode;
-		newNode->before = NULL;
-	}
-		
-	else
-	{
-		newNode->before = st->tail;
-		st->tail->next = newNode;
-	}
-		
-	
-	st->tail = newNode;
-	(st->numOfData)++;
-}
-
-int Size(Stack* st)
-{
-	return st->numOfData;
-}
-
-int Empty(Stack* st)
-{
-	if (Size(st) == 0)
+	if ((cx - x) * (cx - x) + (cy - y) * (cy - y) < cr * cr)
 		return 1;
 	else
 		return 0;
 }
-
-int Top(Stack* st)
+typedef struct _planetary
 {
-	if (Empty(st))
-		return -1;
-	return st->tail->data;
-}
-
-int Pop(Stack* st)
-{
-	if (Empty(st))
-		return -1;
-	Node* dNode;
-	dNode = st->tail;
-	int ddata = dNode->data;
-	
-	if (st->tail->before ==NULL)
-	{
-		st->head = NULL;
-		st->tail = NULL;
-		
-		
-	}
-	else {
-		st->tail = st->tail->before;
-		st->tail->next = NULL;
-	}
-	
-	free(dNode);
-	(st->numOfData)--;
-	
-	return ddata;
-
-}
-int getnum(char input[])
-{
-	int len = strlen(input)-1;
-	double num = 0,i=0;
-	for (;len>=5;len--)
-	{
-		num += pow(10, i) * (input[len] - 48);
-		i++;
-	}
-	return num;
-}
+	int cx;
+	int cy;
+	int cr;
+}Pl;
 int main(void)
 {
-	int iter;
-	scanf("%d",&iter);
-	
-	
-	
-	Stack stack;
-	InitStack(&stack);
-	char input[13];
-	for (int i = 0; i < iter; i++)
+	int i, iter,plnum,x1,x2,y1,y2,count;
+	scanf("%d", &iter);
+	for (i = 0; i < iter; i++)
 	{
-		while (getchar() != '\n');
-		scanf("%[^\n]s",input);
+		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+		Pl pl[50] = { 0,0,0, };
+		scanf("%d", &plnum);
+		count = 0;
+		for (int j = 0; j < plnum; j++)
+		{
+			scanf("%d %d %d", &pl[j].cx, &pl[j].cy, &pl[j].cr);
+			if (Isinpl(pl[j].cx, pl[j].cy, pl[j].cr, x1, y1) && Isinpl(pl[j].cx, pl[j].cy, pl[j].cr, x2, y2))
+				continue;
+			else if (!Isinpl(pl[j].cx, pl[j].cy, pl[j].cr, x1, y1) && !Isinpl(pl[j].cx, pl[j].cy, pl[j].cr, x2, y2))
+				continue;
+			else
+				count++;
+
+		}
+		printf("%d\n", count);
+
 		
-		if (input[1] == 'u')
-			Push(&stack, getnum(input));
-		else if (input[0] == 't')
-			printf("%d\n", Top(&stack));
-		else if (input[0] == 's')
-			printf("%d\n", Size(&stack));
-		else if (input[0] == 'e')
-			printf("%d\n", Empty(&stack));
-		else
-			printf("%d\n", Pop(&stack));
-		
-		
+
 	}
 	return 0;
+	
+	
+
+
+	
 }
--- Personal_Project/Fast_Movie_Select/Crawler.py
+++ Personal_Project/Fast_Movie_Select/Crawler.py
@@ -15,10 +15,16 @@ def get_url():
 class Movie_Url:
     def __init__(self, movie_url):
         self.movie_url = movie_url
+        self.lst = self.navigate_url()
+        self.name_list = self.crawl_movie_name()
+        self.rating_list = self.crawl_movie_rating()
+        self.genre_list, self.director_list, self.actor_list = self.crawl_movie_detail()
+
+    def navigate_url(self):
         response = requests.get(self.movie_url)
         html = BeautifulSoup(response.content, 'html.parser')
         info = html.find('div', {'class': 'main_detail'})
-        self.lst = info.findAll('li')
+        return info.findAll('li')
 
     def crawl_movie_name(self):
         name_list = []
@@ -40,6 +46,7 @@ class Movie_Url:
             img_url = img[img.find('http'):img.find('onload')-2]
 
             request.urlretrieve(img_url, "./image/"+str(self.movie_url[-4:])+str(i+1)+".png")
+        print('Image Downloaded')
 
     def crawl_movie_detail(self):
         base_url = 'https://movie.daum.net'
@@ -58,24 +65,48 @@ class Movie_Url:
             new_info = html.find('div', {'class': 'movie_summary'})
             genre_list.append(new_info.findAll('dd')[0].getText())
             director_list.append(html.find('dd', {'class': 'type_ellipsis'}).find('a').getText())
-            actor_list.append([x.getText() for x in html.findAll('dd',{'class': 'type_ellipsis'})[1]
+            try:
+                actor_list.append([x.getText() for x in html.findAll('dd',{'class': 'type_ellipsis'})[1]
                               .findAll('a')])
+            except IndexError:
+                actor_list.append('X')
 
+        genre_list = [genre.split('/') for genre in genre_list]
 
-        print(len(genre_list))
-        print(len(director_list))
-        print(len(actor_list))
         return genre_list, director_list, actor_list
 
+    def make_movie_df(self):
+        df_movie = pd.DataFrame()
+        df_movie['movie_name'] = self.name_list
+        df_movie['movie_rating'] = self.rating_list
+        df_movie['movie_genre'] = self.genre_list
+        df_movie['movie_director'] = self.director_list
+        df_movie['movie_actor'] = self.actor_list
+
+        return df_movie
+
+def object_column_preprocess(df):
+    df = df.reset_index(drop=True)
+    df['movie_genre_split'] = df['movie_genre'].apply(lambda x: (' ').join(x))
+    df['movie_director_split'] = df['movie_director'].apply(lambda x: x.replace(' ', ''))
+    df['movie_actor_split'] = df['movie_actor'].apply(
+        lambda x: (' ').join([ac.replace(' ', '') for ac in x]))
+
+    return df
 
 
+url_list = get_url()
+for i, year_url in enumerate(url_list):
+    movie = Movie_Url(year_url)
+    # movie.crawl_movie_image()
+    if i == 0:
+        movie_df = movie.make_movie_df()
+    else:
+        movie_df = pd.concat([movie_df, movie.make_movie_df()], axis=0)
 
+    print(i)
 
-
-
-a=get_url()
-b=Movie_Url(a[0])
-# b.crawl_movie_name
-# b.crawl_movie_rating
-# b.crawl_movie_image()
-b.crawl_movie_detail()
+movie_df = object_column_preprocess(movie_df)
+# movie_2004 = Movie_Url(url_list[0])
+# df_2004 = movie_2004.make_movie_df()
+# print(df_2004)

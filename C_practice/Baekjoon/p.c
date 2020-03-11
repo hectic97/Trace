6408f8b55c75650ca75877e7cb434b420aa35c9f
hectic97 <hecticlife@g.skku.edu>
2020-03-11 23:21:10 +0900

    == Today Commit ==

--- C_practice/Baekjoon/p.c
+++ C_practice/Baekjoon/p.c
@@ -1,89 +1,52 @@
-
 #include <stdio.h>
-#include <stdlib.h>
-typedef struct
-{
-	int orgindex;
-	int data;
-}st;
-typedef int Num;
-void MergeTwoArea(Num arr[], int left, int mid, int right, int bydata)
-{
-	int fIdx = left;
-	int rIdx = mid + 1;
-	int i;
-
-	Num* sortArr = (Num*)malloc(sizeof(Num) * (right + 1));
-	int sIdx = left;
-	if (bydata == 0)
-	{
-		while (fIdx <= mid && rIdx <= right)
-		{
-			if (arr[fIdx] <= arr[rIdx])
-				sortArr[sIdx] = arr[fIdx++];
-
-			else
-				sortArr[sIdx] = arr[rIdx++];
-			sIdx++;
-		}
-
-	}
-	else
-	{
-		while (fIdx <= mid && rIdx <= right)
-		{
-			if (arr[fIdx] <= arr[rIdx])
-				sortArr[sIdx] = arr[fIdx++];
-
-			else
-				sortArr[sIdx] = arr[rIdx++];
-			sIdx++;
-		}
-	}
-
-
-
-	if (fIdx > mid)
-	{
-		for (i = rIdx; i <= right; i++, sIdx++)
-			sortArr[sIdx] = arr[i];
-	}
-	else
-	{
-
-		for (i = fIdx; i <= mid; i++, sIdx++)
-			sortArr[sIdx] = arr[i];
-	}
-	for (i = left; i <= right; i++)
-	{
-		arr[i] = sortArr[i];
-	}
-	free(sortArr);
-}
-void MergeSort(Num* arr, int left, int right, int bydata)
-{
-	int mid;
-
-	if (left < right)
-	{
-		mid = (left + right) / 2;
-
-		MergeSort(arr, left, mid, bydata);
-		MergeSort(arr, mid + 1, right, bydata);
-
-		MergeTwoArea(arr, left, mid, right, bydata);
-	}
-}
-
 int main(void)
 {
-	int num[50];
-	int iter;
-	scanf("%d", &iter);
-	for (int i = 0; i < iter; i++)
+	char a[51];
+	char b[51];
+	scanf("%s %s", a, b);
+	int a_len = 0,b_len = 0;
+	while (a[a_len] != 0)
+		a_len++;
+	while (b[b_len] != 0)
+		b_len++;
+	int i,j;
+	int gap = 0;
+	int min = 50;
+	for (j = 0; j <=( a_len>b_len? a_len - b_len:b_len-a_len); j++)
 	{
-		scanf("%d", &num[i]);
+		gap = 0;
+		if (a_len > b_len)
+		{
+			for (i = 0; i < b_len; i++)
+			{
+				
+				if (a[i+j] != b[i])
+					gap++;
+				
+			}
+			if (gap <= min)
+				min = gap;
+			
+		}
+			
+		else if(a_len<=b_len)
+		{
+			for (i = 0; i < a_len; i++)
+			{
+				
+				if (a[i] != b[i+j])
+					gap++;
+				
+			}
+			if (gap <= min)
+				min = gap;
+		}
+		
+
 	}
-	MergeSort(num, 0, iter - 1, 0);
-	printf("%d", num[0] * num[iter-1]);
+	printf("%d", min);
+	return 0;
+
+	
+
 }

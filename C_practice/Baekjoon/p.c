ed051162a5cf35ef7a5f5d5d59b2460b0c6a00ec
hectic97 <hecticlife@g.skku.edu>
2020-03-30 22:40:59 +0900

    == Daily Commit ==

--- C_practice/Baekjoon/p.c
+++ C_practice/Baekjoon/p.c
@@ -1,75 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef int Data;
-void MergeTwoArea(Data arr[], int left, int mid, int  right)
+int top = -1;
+void Push(int* arr, int n)
 {
-	int fidx = left;
-	int ridx = mid + 1;
-	int i;
-
-	Data* sortArr = (Data*)malloc(sizeof(Data) * (right + 1));
-	int sidx = left;
-
-	while (fidx <= mid && ridx <= right)
-	{
-		if (arr[fidx] <= arr[ridx])
-			sortArr[sidx] = arr[fidx++];
-		else
-			sortArr[sidx] = arr[ridx++];
-		sidx++;
-	}
-
-	if (fidx > mid)
-	{
-		for (i = ridx; i <= right; i++, sidx++)
-			sortArr[sidx] = arr[i];
-	}
-	else
-	{
-		for (i = fidx; i <= mid; i++, sidx++)
-			sortArr[sidx] = arr[i];
-	}
-	for (i = left; i <= right; i++)
-		arr[i] = sortArr[i];
-	free(sortArr);
-
+	arr[++top] = n;
 }
-
-void MergeSort(Data arr[], int left, int right)
+void Pop(int* arr)
 {
-	int mid;
-	if (left < right)
-	{
-		mid = (left + right) / 2;
-		MergeSort(arr, left, mid);
-		MergeSort(arr, mid + 1, right);
-		MergeTwoArea(arr, left, mid, right);
-	}
+	top--;
 }
 int main(void)
 {
-	int n, i;
-	scanf("%d", &n);
-	int* arr = (int*)malloc(sizeof(int) * n);
-	for (i = 0; i < n; i++)
-		scanf("%d", &arr[i]);
-	if (n == 1)
+	int k, i,n;
+	scanf("%d", &k);
+	int* arr = (int*)malloc(sizeof(int) * k);
+	for (i = 0; i < k; i++)
 	{
-		printf("%d", arr[0]);
-		return 0;
+		scanf("%d", &n);
+		if (n == 0)
+			Pop(arr);
+		else
+			Push(arr, n);
 	}
-		
-	MergeSort(arr, 0, n - 1);
-	int max = 0;
-	for (i = 0; i < n-1; i++)
-	{
-		if (arr[i] * (n - i) > max)
-			max = arr[i] * (n - i);
-		
-	}
-	printf("%d", max);
+	int sum = 0;
+	for (i = 0; i <= top; i++)
+		sum += arr[i];
+	printf("%d", sum);
 	return 0;
-		
 
-	
 }

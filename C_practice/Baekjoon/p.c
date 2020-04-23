b163871fa43c973e8c2c50b705d9b9dc66b420d2
hectic97 <hecticlife@g.skku.edu>
2020-04-23 22:20:19 +0900

    == Daily Commit ==

--- C_practice/Baekjoon/p.c
+++ C_practice/Baekjoon/p.c
@@ -1,118 +1,383 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
 
-typedef struct _Node 
-{ 
-	int data; 
-	struct _Node* next; 
+typedef struct _st
+{
+	int coef;
+	int exp;
+}Ce;
+
+typedef Ce Data;
+
+void MergeTwoArea(Data arr[], int left, int mid, int  right)
+{
+	int fidx = left;
+	int ridx = mid + 1;
+	int i;
+
+	Data* sortArr = (Data*)malloc(sizeof(Data) * (right + 1));
+	int sidx = left;
+
+	while (fidx <= mid && ridx <= right)
+	{
+		if (arr[fidx].exp >= arr[ridx].exp)
+			sortArr[sidx] = arr[fidx++];
+		else
+			sortArr[sidx] = arr[ridx++];
+		sidx++;
+	}
+
+	if (fidx > mid)
+	{
+		for (i = ridx; i <= right; i++, sidx++)
+			sortArr[sidx] = arr[i];
+	}
+	else
+	{
+		for (i = fidx; i <= mid; i++, sidx++)
+			sortArr[sidx] = arr[i];
+	}
+	for (i = left; i <= right; i++)
+		arr[i] = sortArr[i];
+	free(sortArr);
+
+}
+
+void MergeSort(Data arr[], int left, int right)
+{
+	int mid;
+	if (left < right)
+	{
+		mid = (left + right) / 2;
+		MergeSort(arr, left, mid);
+		MergeSort(arr, mid + 1, right);
+		MergeTwoArea(arr, left, mid, right);
+	}
+}
+typedef struct _node
+{
+	int coef;
+	int exp;
+	
+	struct _node* next;
 }Node;
 
-void printList(Node* head)
+typedef struct linkedlist
 {
-	Node* ptr = head;
-	while (ptr)
-	{
-		printf("%d ", ptr->data);
-		printf("-> ");
-		ptr = ptr->next;
-	}
-	printf("NULL");
-	printf("\n");
+	Node* head;
+	Node* tail;
+	int numofData;
+}List;
+
+int Empty(List* lst)
+{
+	if (lst->numofData == 0)
+		return 1;
+	else
+		return 0;
 }
-void push(Node** head, int data)
+void initLinkedList(List *lst)
+{
+	lst->numofData = 0;
+	lst->tail = NULL;
+	lst->head = NULL;
+}
+
+void Push(List* lst, int coef, int exp)
 {
 	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->data = data;
-	newNode->next = *head; 
-	*head = newNode;
-}
-Node* Reserve(Node* head, int g)
-{
-	int i, j,temp;
-	int remainder = 0;       
-	Node* from = head;
-	Node* end;
-	Node* freeptr = head;
-	Node* start;
+	newNode->coef = coef;
+	newNode->exp = exp;
 	
-	while (remainder == 0)  //remainder == 0 means absoulte part
+	
+	if (Empty(lst))
 	{
-		freeptr = from;
+		lst->head = newNode;
+		lst->tail = newNode;
 		
-		int count = 1;
-		for (i = 0; i < g-1; i++)
-		{
-			if (freeptr->next == NULL)
-			{
-				remainder = 1;
-				break;
-			}
-			freeptr = freeptr->next;
-			count++;
-		}
-		
-		if (remainder == 0)
-		{
-			end = freeptr;
-			
-			start = from;
-			for (i = 0; i < g / 2; i++)
-			{
-				
-				freeptr = start;
-				temp = from->data;
-				for (j = 0; j < g - i-1; j++)
-					freeptr = freeptr->next;
-				from->data = freeptr->data;
-				
-				freeptr->data = temp;
-				
-				from = from->next;
-				
-			}
-			if (end->next == NULL)
-				remainder = 1;
-			else
-				from = end->next;
+	}
+	else
+	{
+		lst->tail->next = newNode;
+		lst->tail = newNode;
+	}
+	newNode->next = NULL;
+	(lst->numofData)++;
+}
 
+void PrintList(List* lst)
+{
+	Node* ptr = lst->head;
+	if (Empty(lst))
+	{
+		printf("Empty List\n");
+		return;
+	}
+	while (ptr != NULL)
+	{
+		if (ptr->coef == 0)
+		{
+			
+			if (ptr == lst->head)
+			{
+				lst->head =lst->head->next;
+				
+			}
+			
+			
+		}
+		else if (ptr->coef == 1)
+		{
+			if (ptr == lst->head)
+			{
+				
+					printf("x^%d", ptr->exp);
+				
+			}
+			else
+			{
+				if (ptr->coef >= 0)
+					printf("+x^%d", ptr->exp);
+				
+			}
+			
+		}
+		else if (ptr->coef == -1)
+		{
+			if (ptr == lst->head)
+			{
+				
+				
+					printf("-x^%d",  ptr->exp);
+			}
+			else
+			{
+				
+					printf("-x^%d",  ptr->exp);
+			}
 		}
 		else
 		{
-			end = freeptr;
-			freeptr = from;
-
-			for (i = 0; i < count / 2; i++)
+			if (ptr == lst->head)
 			{
-				freeptr = from;
-				temp = from->data;
-				for (j = 0; j < count - i-1; j++)
-					freeptr = freeptr->next;
-				from->data = freeptr->data;
-				freeptr->data = temp;
-				from = from->next;
+				if (ptr->coef >= 0)
+					printf("%dx^%d", ptr->coef, ptr->exp);
+				else
+					printf("%dx^%d", ptr->coef, ptr->exp);
 			}
-			
+			else
+			{
+				if (ptr->coef >= 0)
+					printf("+%dx^%d", ptr->coef, ptr->exp);
+				else
+					printf("%dx^%d", ptr->coef, ptr->exp);
+			}
 		}
 		
+		ptr = ptr->next;
+			
 	}
-	
-	return head;
-	
-	
 
 }
-int main()
+Ce Delete(List* lst)
 {
-	int N, list[100], g;
-	scanf("%d", &N);
-	for (int i = 0; i < N; i++)
-		scanf("%d", &list[i]);
-	Node* head = NULL;
-	scanf("%d", &g);
-	for (int i = N - 1; i >= 0; i--)
-		push(&head, list[i]);
-	printList(head);
-	head = Reserve(head, g);
-	printList(head);
+	Node* rpos = lst->head;
+	Ce rdata = { rpos->coef,rpos->exp };
+
+	if (Empty(lst))
+	{
+		printf("error");
+		return rdata;
+	}
+	else if (rpos->next == NULL)
+	{
+		lst->head = NULL;
+		lst->tail = NULL;
+	}
+	else
+	{
+		lst->head = lst->head->next;
+	}
+	free(rpos);
+	(lst->numofData)--;
+	return rdata;
+		
+		
+}
+void Merge2List(List* lst1, List* lst2, List* resultlst)
+{
+	Node* ptr1 = lst1->head;
+	Node* ptr2 = lst2->head;
+	Ce deldata,deldata2;
+	
+	while (1)
+	{
+		if (ptr1->exp > ptr2->exp)
+		{
+			deldata = Delete(lst1);
+			Push(resultlst, deldata.coef, deldata.exp);
+			ptr1 = lst1->head;
+		}
+		else if (ptr1->exp < ptr2->exp)
+		{
+			deldata2 = Delete(lst2);
+			Push(resultlst, deldata2.coef, deldata2.exp);
+			ptr2 = lst2->head;
+		}
+		else
+		{
+			deldata = Delete(lst1);
+			deldata2 = Delete(lst2);
+			Push(resultlst, deldata.coef + deldata2.coef, deldata.exp);
+			ptr1 = lst1->head;
+			ptr2 = lst2->head;
+		}
+		if (ptr1 == NULL)
+		{
+			while (lst2->numofData != 0)
+			{
+				deldata2 = Delete(lst2);
+				Push(resultlst, deldata2.coef, deldata2.exp);
+				ptr2 = lst2->head;
+			}
+			break;
+		}
+		if (ptr2 == NULL)
+		{
+			while (lst1->numofData != 0)
+			{
+				deldata = Delete(lst1);
+				Push(resultlst, deldata.coef, deldata.exp);
+				ptr1 = lst1->head;
+			}
+			break;
+		}
+	}
+	
+	
+}
+
+int main(void)
+{
+	char ch1[1000];
+	int arr[100];
+	char ch2[1000];
+	int arr2[100];
+	scanf("%s", ch1);
+	scanf("%s", ch2);
+	int size = strlen(ch1);
+	int i;
+	int j = 0;
+	for (i = 0; i < size; i++)
+	{
+		if (i==0 && ch1[i] == 'x')
+		{
+			arr[j++] = 1;
+			
+			continue;
+		}
+		if (ch1[i - 1] == '+' && ch1[i] == 'x')
+		{
+			arr[j++] = 1;
+			continue;
+		}
+			
+		if (ch1[i - 1] == '-' && ch1[i] == 'x')
+		{
+			arr[j++] = -1;
+			continue;
+		}
+			
+		if (i > 0 && isdigit(ch1[i - 1]))
+			continue;
+		if (isdigit(ch1[i]))
+		{
+			int num = strtol(&ch1[i], NULL, 10);
+			if (i > 0 && ch1[i - 1] == '-')
+				num *= -1;
+			arr[j++] = num;
+		}
+	}
+	int arrlen = j;
+	j = 0;
+	size = strlen(ch2);
+	for (i = 0; i < size; i++)
+	{
+		if (i==0 && ch2[0] == 'x')
+		{
+			arr2[j++] = 1;
+			continue;
+		}
+		if (ch2[i - 1] == '+' && ch2[i] == 'x')
+			arr2[j++] = 1;
+		if (ch2[i - 1] == '-' && ch2[i] == 'x')
+			arr2[j++] = -1;
+		if (i > 0 && isdigit(ch2[i - 1]))
+			continue;
+		if (isdigit(ch2[i]))
+		{
+			int num2 = strtol(&ch2[i], NULL, 10);
+			if (i > 0 && ch2[i - 1] == '-')
+				num2 *= -1;
+			arr2[j++] = num2;
+		}
+	}
+	for (i = 0; i < arrlen; i++)
+		printf("arr:%d ", arr[i]);
+	int arr2len = j;
+	Ce ce1[50];
+	Ce ce2[50];
+	for (j=0,i = 0; i < arrlen;)
+	{
+		ce1[j].coef = arr[i++];
+		ce1[j++].exp = arr[i++];
+	}
+	for (j=0,i = 0; i < arr2len;)
+	{
+		ce2[j].coef = arr2[i++];
+		ce2[j++].exp = arr2[i++];
+	}
+	MergeSort(ce1, 0, arrlen / 2 - 1);
+	MergeSort(ce2, 0, arr2len / 2 - 1);
+	for (j=0,i = 0; i < arrlen; )
+	{
+		arr[i++] = ce1[j].coef;
+		arr[i++] = ce1[j++].exp;
+	}
+	for (j = 0, i = 0; i < arr2len; )
+	{
+		arr2[i++] = ce2[j].coef;
+		arr2[i++] = ce2[j++].exp;
+	}
+	
+	List lst1, lst2,resultlst;
+	initLinkedList(&lst1);
+	initLinkedList(&lst2);
+	initLinkedList(&resultlst);
+	
+	for (i = 0; i < arrlen;)
+	{
+		Push(&lst1, arr[i], arr[i + 1]);
+		i += 2;
+	}
+	for (i = 0; i < arr2len;)
+	{
+		Push(&lst2, arr2[i], arr2[i + 1]);
+		i += 2;
+	}
+	
+	PrintList(&lst1);
+	printf("\n");
+	PrintList(&lst2);
+	printf("\n");
+	Merge2List(&lst1, &lst2, &resultlst);
+	PrintList(&resultlst);
 	return 0;
+
+
+	
+		
 }
---
+++ Junior_1st_Semester/Data_Structure/hw/2016314726_HW2C.c
@@ -0,0 +1,382 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+typedef struct _st
+{
+	int coef;
+	int exp;
+}Ce;
+
+typedef Ce Data;
+
+void MergeTwoArea(Data arr[], int left, int mid, int  right)
+{
+	int fidx = left;
+	int ridx = mid + 1;
+	int i;
+
+	Data* sortArr = (Data*)malloc(sizeof(Data) * (right + 1));
+	int sidx = left;
+
+	while (fidx <= mid && ridx <= right)
+	{
+		if (arr[fidx].exp >= arr[ridx].exp)
+			sortArr[sidx] = arr[fidx++];
+		else
+			sortArr[sidx] = arr[ridx++];
+		sidx++;
+	}
+
+	if (fidx > mid)
+	{
+		for (i = ridx; i <= right; i++, sidx++)
+			sortArr[sidx] = arr[i];
+	}
+	else
+	{
+		for (i = fidx; i <= mid; i++, sidx++)
+			sortArr[sidx] = arr[i];
+	}
+	for (i = left; i <= right; i++)
+		arr[i] = sortArr[i];
+	free(sortArr);
+
+}
+
+void MergeSort(Data arr[], int left, int right)
+{
+	int mid;
+	if (left < right)
+	{
+		mid = (left + right) / 2;
+		MergeSort(arr, left, mid);
+		MergeSort(arr, mid + 1, right);
+		MergeTwoArea(arr, left, mid, right);
+	}
+}
+typedef struct _node
+{
+	int coef;
+	int exp;
+	
+	struct _node* next;
+}Node;
+
+typedef struct linkedlist
+{
+	Node* head;
+	Node* tail;
+	int numofData;
+}List;
+
+int Empty(List* lst)
+{
+	if (lst->numofData == 0)
+		return 1;
+	else
+		return 0;
+}
+void initLinkedList(List *lst)
+{
+	lst->numofData = 0;
+	lst->tail = NULL;
+	lst->head = NULL;
+}
+
+void Push(List* lst, int coef, int exp)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->coef = coef;
+	newNode->exp = exp;
+	
+	
+	if (Empty(lst))
+	{
+		lst->head = newNode;
+		lst->tail = newNode;
+		
+	}
+	else
+	{
+		lst->tail->next = newNode;
+		lst->tail = newNode;
+	}
+	newNode->next = NULL;
+	(lst->numofData)++;
+}
+
+void PrintList(List* lst)
+{
+	Node* ptr = lst->head;
+	if (Empty(lst))
+	{
+		printf("Empty List\n");
+		return;
+	}
+	while (ptr != NULL)
+	{
+		if (ptr->coef == 0)
+		{
+			
+			if (ptr == lst->head)
+			{
+				lst->head =lst->head->next;
+				
+			}
+			
+			
+		}
+		else if (ptr->coef == 1)
+		{
+			if (ptr == lst->head)
+			{
+				
+					printf("x^%d", ptr->exp);
+				
+			}
+			else
+			{
+				if (ptr->coef >= 0)
+					printf("+x^%d", ptr->exp);
+				
+			}
+			
+		}
+		else if (ptr->coef == -1)
+		{
+			if (ptr == lst->head)
+			{
+				
+				
+					printf("-x^%d",  ptr->exp);
+			}
+			else
+			{
+				
+					printf("-x^%d",  ptr->exp);
+			}
+		}
+		else
+		{
+			if (ptr == lst->head)
+			{
+				if (ptr->coef >= 0)
+					printf("%dx^%d", ptr->coef, ptr->exp);
+				else
+					printf("%dx^%d", ptr->coef, ptr->exp);
+			}
+			else
+			{
+				if (ptr->coef >= 0)
+					printf("+%dx^%d", ptr->coef, ptr->exp);
+				else
+					printf("%dx^%d", ptr->coef, ptr->exp);
+			}
+		}
+		
+		ptr = ptr->next;
+			
+	}
+
+}
+Ce Delete(List* lst)
+{
+	Node* rpos = lst->head;
+	Ce rdata = { rpos->coef,rpos->exp };
+
+	if (Empty(lst))
+	{
+		printf("error");
+		return rdata;
+	}
+	else if (rpos->next == NULL)
+	{
+		lst->head = NULL;
+		lst->tail = NULL;
+	}
+	else
+	{
+		lst->head = lst->head->next;
+	}
+	free(rpos);
+	(lst->numofData)--;
+	return rdata;
+		
+		
+}
+void Merge2List(List* lst1, List* lst2, List* resultlst)
+{
+	Node* ptr1 = lst1->head;
+	Node* ptr2 = lst2->head;
+	Ce deldata,deldata2;
+	
+	while (1)
+	{
+		if (ptr1->exp > ptr2->exp)
+		{
+			deldata = Delete(lst1);
+			Push(resultlst, deldata.coef, deldata.exp);
+			ptr1 = lst1->head;
+		}
+		else if (ptr1->exp < ptr2->exp)
+		{
+			deldata2 = Delete(lst2);
+			Push(resultlst, deldata2.coef, deldata2.exp);
+			ptr2 = lst2->head;
+		}
+		else
+		{
+			deldata = Delete(lst1);
+			deldata2 = Delete(lst2);
+			Push(resultlst, deldata.coef + deldata2.coef, deldata.exp);
+			ptr1 = lst1->head;
+			ptr2 = lst2->head;
+		}
+		if (ptr1 == NULL)
+		{
+			while (lst2->numofData != 0)
+			{
+				deldata2 = Delete(lst2);
+				Push(resultlst, deldata2.coef, deldata2.exp);
+				ptr2 = lst2->head;
+			}
+			break;
+		}
+		if (ptr2 == NULL)
+		{
+			while (lst1->numofData != 0)
+			{
+				deldata = Delete(lst1);
+				Push(resultlst, deldata.coef, deldata.exp);
+				ptr1 = lst1->head;
+			}
+			break;
+		}
+	}
+	
+	
+}
+
+int main(void)
+{
+	char ch1[1000];
+	int arr[100];
+	char ch2[1000];
+	int arr2[100];
+	scanf("%s", ch1);
+	scanf("%s", ch2);
+	int size = strlen(ch1);
+	int i;
+	int j = 0;
+	for (i = 0; i < size; i++)
+	{
+		if (i==0 && ch1[i] == 'x')
+		{
+			arr[j++] = 1;
+			
+			continue;
+		}
+		if (ch1[i - 1] == '+' && ch1[i] == 'x')
+		{
+			arr[j++] = 1;
+			continue;
+		}
+			
+		if (ch1[i - 1] == '-' && ch1[i] == 'x')
+		{
+			arr[j++] = -1;
+			continue;
+		}
+			
+		if (i > 0 && isdigit(ch1[i - 1]))
+			continue;
+		if (isdigit(ch1[i]))
+		{
+			int num = strtol(&ch1[i], NULL, 10);
+			if (i > 0 && ch1[i - 1] == '-')
+				num *= -1;
+			arr[j++] = num;
+		}
+	}
+	int arrlen = j;
+	j = 0;
+	size = strlen(ch2);
+	for (i = 0; i < size; i++)
+	{
+		if (i==0 && ch2[0] == 'x')
+		{
+			arr2[j++] = 1;
+			continue;
+		}
+		if (ch2[i - 1] == '+' && ch2[i] == 'x')
+			arr2[j++] = 1;
+		if (ch2[i - 1] == '-' && ch2[i] == 'x')
+			arr2[j++] = -1;
+		if (i > 0 && isdigit(ch2[i - 1]))
+			continue;
+		if (isdigit(ch2[i]))
+		{
+			int num2 = strtol(&ch2[i], NULL, 10);
+			if (i > 0 && ch2[i - 1] == '-')
+				num2 *= -1;
+			arr2[j++] = num2;
+		}
+	}
+	
+	int arr2len = j;
+	Ce ce1[50];
+	Ce ce2[50];
+	for (j=0,i = 0; i < arrlen;)
+	{
+		ce1[j].coef = arr[i++];
+		ce1[j++].exp = arr[i++];
+	}
+	for (j=0,i = 0; i < arr2len;)
+	{
+		ce2[j].coef = arr2[i++];
+		ce2[j++].exp = arr2[i++];
+	}
+	MergeSort(ce1, 0, arrlen / 2 - 1);
+	MergeSort(ce2, 0, arr2len / 2 - 1);
+	for (j=0,i = 0; i < arrlen; )
+	{
+		arr[i++] = ce1[j].coef;
+		arr[i++] = ce1[j++].exp;
+	}
+	for (j = 0, i = 0; i < arr2len; )
+	{
+		arr2[i++] = ce2[j].coef;
+		arr2[i++] = ce2[j++].exp;
+	}
+	
+	List lst1, lst2,resultlst;
+	initLinkedList(&lst1);
+	initLinkedList(&lst2);
+	initLinkedList(&resultlst);
+	
+	for (i = 0; i < arrlen;)
+	{
+		Push(&lst1, arr[i], arr[i + 1]);
+		i += 2;
+	}
+	for (i = 0; i < arr2len;)
+	{
+		Push(&lst2, arr2[i], arr2[i + 1]);
+		i += 2;
+	}
+	
+	//printlist(&lst1);
+	//printf("\n");
+	//printlist(&lst2);
+	//printf("\n");
+	Merge2List(&lst1, &lst2, &resultlst);
+	PrintList(&resultlst);
+	return 0;
+
+
+	
+		
+}

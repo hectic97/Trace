544ce84de9f929c48ee23a78cca920c389829a65
hectic97 <hecticlife@g.skku.edu>
2020-06-29 00:00:02 +0900

    :)

--- Kotlin/src/Hello_Kotlin.kt
+++ Kotlin/src/Hello_Kotlin.kt
@@ -1,4 +1,23 @@
+fun printMessage(message: String):Unit{
+    println(message)
+}
+
+fun sum(x:int,y:int){
+    return x+y
+}
+
+fun multiply(x:int,y:int){
+    return x*y
+}
+
+fun printMessageWithPrefix(message:String,prefix:String="Info"){
+    println("[$prefix] $message")
+}
 fun main()
 {
-    println("Hello, Kotlin!")
+    printMessage("hello_Kotlin")
+    printMessageWithPrefix("Hello","Log")
+    printMessageWithPrefix(prefix = "Log",message = "Hello")
+    println(sum(1,2))
+    println(multiply(4,5))
 }

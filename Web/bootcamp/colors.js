e85c3b0c0ee78bc907ecec931cfe5d6d3693bcb3
hectic97 <hecticlife@g.skku.edu>
2020-07-04 01:22:06 +0900

    'friday'

--- Web/bootcamp/colors.js
+++ Web/bootcamp/colors.js
@@ -1,39 +1,37 @@
-
-var h2tagman = {
-    setColor:function(color){
-    var alist = document.querySelectorAll('h2');
-    var i = 0;
-    while(i<alist.length){
-        alist[i].style.color = color;
-        i++;
-    } 
-}
+var Links = {
+  setColor:function(color){
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // }
+    $('a').css('color', color);
+  }
 }
 var Body = {
-    setColor:function(color){
-    document.querySelector('body').style.color = color
-    },
-    setBackgroundColor:function(color){
-    document.querySelector('body').style.backgroundColor = color;
-    }
+  setColor:function (color){
+    //document.querySelector('body').style.color = color;
+    $('body').css('color', color);
+  },
+  setBackgroundColor:function (color){
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
+  }
 }
-
-
 function nightDayHandler(self){
-    var target = document.querySelector('body');
-    if(self.value === 'night'){
-        Body.setBackgroundColor('black');
-        Body.setColor('white');
-        self.value = 'day';
-        h2tagman.setColor('crimson');
-        
-    }
-    else{
-        Body.setBackgroundColor('white');
-        Body.setColor('black');
-        self.value = 'night';
-        h2tagman.setColor('blue');
+  var target = document.querySelector('body');
+  if(self.value === 'night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
 
-    }
-            
+    Links.setColor('white');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue');
+  }
 }

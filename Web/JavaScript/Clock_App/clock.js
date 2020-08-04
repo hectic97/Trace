84ff488e97e8027836d1352fc4d7a8fb64f97589
hectic97 <hecticlife@g.skku.edu>
2020-08-05 02:11:24 +0900

    Dawn JS coding..

--- Web/JavaScript/Clock_App/clock.js
+++ Web/JavaScript/Clock_App/clock.js
@@ -1,13 +1,17 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
-
+function fixTimeLength(time){
+    if (time < 10){
+        time = `0` + time;
+    };
+    return time;
+}
 function getTime(){
     const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
+    const minutes = fixTimeLength(date.getMinutes());
+    const hours = fixTimeLength(date.getHours());
+    const seconds = fixTimeLength(date.getSeconds());
     clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
-    
 }
 
 function init() {
